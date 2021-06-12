63185d6890065e0f53465e23eb0d894f145b27af
chengyehwang <cywang.eda@gmail.com>
2021-06-12 12:53:13 +0800

    gcode as library

--- control.py
+++ control.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 import re
 from subprocess import PIPE, Popen
@@ -23,7 +23,7 @@ def adb_cmd(cmd, **kwargs):
         stdout=PIPE,
         shell=True
     ).stdout
-    return process.read().decode().split('\r\n')
+    return process.read().decode().split(os.linesep)
 def adb_shell (cmd, **kwargs):
     dut = kwargs.get('dut',False)
     mon = kwargs.get('mon',False)
@@ -32,32 +32,78 @@ def adb_shell (cmd, **kwargs):
     if dut and not device_dut or mon and not device_mon:
         outs = adb_cmd('devices')
         for line in outs:
-            m = re.search('(\S+)\s+device', line)
+            print('line', line)
+            m = re.search('(\S+)\s+device$', line)
             if m:
                 device = m.group(1)
                 if len(device) > 10:
                     device_dut = device
                 else:
                     device_mon = device
-        print(device_dut, device_mon)
+        print('dut', device_dut, 'mon', device_mon)
     
     if dut:
         return adb_cmd('-s ' + device_dut + ' shell "' + cmd + '"')
     if mon:
         return adb_cmd('-s ' + device_mon + ' shell "' + cmd + '"')
 
+def camera_file(cmd):
+    dir = '/sdcard/DCIM/Camera'
+    out = adb_mon('ls -t %s'%dir)
+    file = out[0]
+    adb_cmd('pull %s/%s'%(dir,file),mon=True)
+
 def adb_dut(cmd):
     return adb_shell(cmd, dut = True)
 def adb_mon(cmd):
     return adb_shell(cmd, mon = True)
 
+#camera_file('')
+
+def list_package(name):
+    packages = adb_dut('pm list packages -f')
+    for p in packages:
+        m = re.search('.*=(.*)',p)
+        if m:
+            package = m.group(1)
+            if name in package:
+                return (package)
+
+def fps_begin():
+    package = list_package('baidu')
+    adb_dut('dumpsys gfxinfo %s reset'%package)
+def fps_end():
+    package = list_package('baidu')
+    out = adb_dut('dumpsys gfxinfo %s'%package)
+    with open('gfxinfo','w') as fp:
+        fp.write('\n'.join(out))
+
 #adb_mon('input keyevent POWER')
-adb_mon('input keyevent WAKEUP')
-adb_mon('input keyevent FOCUS')
-adb_mon('input keyevent KEYCODE_CAMERA')
-adb_mon('input swipe 300 1000 300 100')
-print(adb_mon('am start -a android.media.action.IMAGE_CAPTURE --ei android.intent.extras.CAMERA_FACING 0'))
-adb_cmd('push scroll.sh /data/local/tmp', dut=True)
-adb_dut('chmod 755 /data/local/tmp/scroll.sh')
-adb_dut('dos2unix /data/local/tmp/scroll.sh')
-adb_dut('nohup /data/local/tmp/scroll.sh >/dev/null 2>/dev/null &')
+#adb_mon('input keyevent WAKEUP')
+#adb_mon('input keyevent FOCUS')
+#adb_mon('input keyevent KEYCODE_CAMERA')
+#adb_mon('input swipe 300 1000 300 100')
+#print(adb_mon('am start -a android.media.action.IMAGE_CAPTURE --ei android.intent.extras.CAMERA_FACING 0'))
+if False:
+    adb_cmd('push scroll.sh /data/local/tmp', dut=True)
+    adb_dut('chmod 755 /data/local/tmp/scroll.sh')
+    adb_dut('dos2unix /data/local/tmp/scroll.sh')
+    adb_dut('nohup /data/local/tmp/scroll.sh >/dev/null 2>/dev/null &')
+fps_begin()
+if True:
+    with open("scroll_test.gcode", "w") as fp:
+        fp.write("""$x
+$h
+G92 X0Y0Z0
+G90
+""")
+        for i in range(20):
+            fp.write("""
+G01 X50Y-120F10000
+G01 Z-20F1000
+G01 X50Y-30F12000
+G01 Z0F4000
+""")
+
+    os.system('python3 gcode.py --gcode_file scroll_test.gcode')
+fps_end()
--- gcode.py
+++ gcode.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """https://github.com/gnea/grbl/blob/v1.1h.20190825/doc/script/stream.py"""
 import serial
 import re
@@ -7,43 +7,50 @@ import sys
 import argparse
 import threading
 
-DEVICE_FILE = 'COM5'
 RX_BUFFER_SIZE = 128
-BAUD_RATE = 115200
 
+def gcode_begin():
+    #DEVICE_FILE = 'COM5'
+    DEVICE_FILE = '/dev/ttyACM0'
+    BAUD_RATE = 115200
 
-# Define command line argument interface
-parser = argparse.ArgumentParser(description='python library')
-parser.add_argument('gcode_file', type=argparse.FileType('r'),
-        help='g-code filename to be streamed')
-parser.add_argument('-q','--quiet',action='store_true', default=False, 
-        help='suppress output text')
-args = parser.parse_args()
+    # Initialize
+    global s
+    s = serial.Serial(DEVICE_FILE, BAUD_RATE)
 
-# Initialize
-s = serial.Serial(DEVICE_FILE, BAUD_RATE)
-f = args.gcode_file
+    # Wake up grbl
+    print ("Initializing Grbl...")
+    s.write(bytes("\r\n\r\n",'ascii'))
 
-# Wake up grbl
-print ("Initializing Grbl...")
-s.write(bytes("\r\n\r\n",'ascii'))
-
-# Wait for grbl to initialize and flush startup text in serial input
-time.sleep(2)
-s.flushInput()
-start_time = time.time();
-c_count = 0
-error_count = 0
-if True:
-    g_count = 0
-    c_line = []
-    for line in f:
-        c_count += 1 # Iterate line counter
-        l_block = re.sub('\s|\(.*?\)','',line).upper() # Strip comments/spaces/new line and capitalize
-        # l_block = line.strip()
-        c_line.append(len(l_block)+1) # Track number of characters in grbl serial read buffer
-        grbl_out = '' 
-        while sum(c_line) >= RX_BUFFER_SIZE-1 | s.inWaiting() :
+    # Wait for grbl to initialize and flush startup text in serial input
+    time.sleep(2)
+    s.flushInput()
+def gcode_kernel(lines):
+    c_count = 0
+    error_count = 0
+    if True:
+        g_count = 0
+        c_line = []
+        for line in lines:
+            c_count += 1 # Iterate line counter
+            l_block = re.sub('\s|\(.*?\)','',line).upper() # Strip comments/spaces/new line and capitalize
+            # l_block = line.strip()
+            c_line.append(len(l_block)+1) # Track number of characters in grbl serial read buffer
+            grbl_out = '' 
+            while sum(c_line) >= RX_BUFFER_SIZE-1 | s.inWaiting() :
+                out_temp = s.readline().strip().decode('ascii') # Wait for grbl response
+                if out_temp.find('ok') < 0 and out_temp.find('error') < 0 :
+                    print ("    MSG: \""+out_temp+"\"") # Debug response
+                else :
+                    if out_temp.find('error') >= 0 :
+                        error_count += 1
+                    g_count += 1 # Iterate g-code counter
+                    print ("  REC<"+str(g_count)+": \""+out_temp+"\"")
+                    del c_line[0] # Delete the block character count corresponding to the last 'ok'
+            s.write(bytes(l_block + '\n','ascii')) # Send g-code block to grbl
+            print ("SND>"+str(c_count)+": \"" + l_block + "\"")
+        # Wait until all responses have been received.
+        while c_count > g_count :
             out_temp = s.readline().strip().decode('ascii') # Wait for grbl response
             if out_temp.find('ok') < 0 and out_temp.find('error') < 0 :
                 print ("    MSG: \""+out_temp+"\"") # Debug response
@@ -51,27 +58,29 @@ if True:
                 if out_temp.find('error') >= 0 :
                     error_count += 1
                 g_count += 1 # Iterate g-code counter
-                print ("  REC<"+str(g_count)+": \""+out_temp+"\"")
                 del c_line[0] # Delete the block character count corresponding to the last 'ok'
-        s.write(bytes(l_block + '\n','ascii')) # Send g-code block to grbl
-        print ("SND>"+str(c_count)+": \"" + l_block + "\"")
-    # Wait until all responses have been received.
-    while c_count > g_count :
-        out_temp = s.readline().strip().decode('ascii') # Wait for grbl response
-        if out_temp.find('ok') < 0 and out_temp.find('error') < 0 :
-            print ("    MSG: \""+out_temp+"\"") # Debug response
-        else :
-            if out_temp.find('error') >= 0 :
-                error_count += 1
-            g_count += 1 # Iterate g-code counter
-            del c_line[0] # Delete the block character count corresponding to the last 'ok'
-            print ("  REC<"+str(g_count)+": \""+out_temp + "\"")
+                print ("  REC<"+str(g_count)+": \""+out_temp + "\"")
 
-# Wait for user input after streaming is completed
-print ("\nG-code streaming finished!")
-end_time = time.time();
-print (" Time elapsed: ",end_time-start_time,"\n")
+    # Wait for user input after streaming is completed
+def gcode_end():
+    print ("\nG-code streaming finished!")
 
-# Close file and serial port
-f.close()
-s.close()
+    # Close file and serial port
+    s.close()
+
+# Define command line argument interface
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='python library')
+    parser.add_argument('--gcode_file', dest='gcode_file',
+        help='g-code filename to be streamed')
+    parser.add_argument('-q','--quiet',action='store_true', default=False, 
+        help='suppress output text')
+    args = parser.parse_args()
+
+    gcode_begin()
+
+    with open(args.gcode_file) as fp:
+        lines = fp.readlines()
+    gcode_kernel(lines)
+    gcode_end()
