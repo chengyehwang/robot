f17fd07d959333fdb1897e9c53aef1115fc0b2da
ChengYehWang <cywang.eda@gmail.com>
2021-06-14 07:49:32 +0800

    fork process to record video

--- run.py
+++ run.py
@@ -2,19 +2,25 @@
 from camera_calib import *
 from control import *
 from gcode import *
-
+from arm import *
+from multiprocessing import Process
+import os
 if True:
-    if True:
+    if False:
         calib_do()
         calib_post()
+    if True:
         gcode_begin()
         gcode_kernel("$x\n$h\nG92 X0Y0Z0\nG90\n".split('\n'))
         fps_begin()
         launch_package()
+        p = Process(video, args = ('test.avi')
+        p.start()
         for i in range(20):
             pass
             gcode_kernel("G01 X230Y-120F10000\nG01 Z-20F1000\nG01 X230Y-30F12000\nG01 Z0F4000\n".split('\n'))
 
+        p.join()
         fps_end()
         fps_plot()
 
