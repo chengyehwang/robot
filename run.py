1320184764a5ef6b7c130b9b38584357e33c0d36
ChengYehWang <cywang.eda@gmail.com>
2021-06-21 05:59:30 +0800

    pos

--- run.py
+++ run.py
@@ -6,27 +6,28 @@ from arm import *
 from multiprocessing import Process
 from motion import *
 import os
-if True:
+if False:
     gcode_begin()
     gcode_kernel("$x\n$h\nG92 X0Y0Z0\nG90\n".split('\n'))
     gcode_end()
     calib_do()
     calib_post()
-if False:
+if True:
     gcode_begin()
     gcode_kernel("$x\n$h\nG92 X0Y0Z0\nG90\n".split('\n'))
+    gcode_kernel(["G01 Z-15F4000"])
     fps_begin()
     launch_package()
     p = Process(target = video, args = ('test.avi',))
     p.start()
     for i in range(20):
         pass
-        gcode_kernel("G01 X230Y-120F10000\nG01 Z-20F1000\nG01 X230Y-30F5000\nG01 Z0F4000\n".split('\n'))
+        gcode_kernel("G01 X230Y-120F10000\nG01 Z-20F1000\nG01 X230Y-30F5000\nG01 Z-15F4000\n".split('\n'))
 
     p.join()
     fps_end()
     fps_plot()
 
-if True:
+if False:
     track()
 
